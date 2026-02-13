5b55a808286b6caa4f3baa44ec1084d3f9856c29
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-13 09:47:54 -0300

    feat: refac: Produto controller, add Produto DTO

--- src/main/java/com/github/renanlmv/ms/produto/controller/ProdutoController.java
+++ src/main/java/com/github/renanlmv/ms/produto/controller/ProdutoController.java
@@ -2,28 +2,14 @@ package com.github.renanlmv.ms.produto.controller;
 
 import com.github.renanlmv.ms.produto.dto.ProdutoInputDTO;
 import com.github.renanlmv.ms.produto.dto.ProdutoResponseDTO;
-import com.github.renanlmv.ms.produto.entities.Produto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-    @GetMapping
-    public ResponseEntity<List<ProdutoResponseDTO>> getProduto() {
 
-        List<ProdutoResponseDTO> dto = ProdutoResponseDTO.createMock();
-        return ResponseEntity.ok(dto);
-    }
-
-    @PostMapping
-    public ResponseEntity<ProdutoResponseDTO> createProduto(@RequestBody ProdutoInputDTO inputDTO) {
-
-        ProdutoResponseDTO dto = new ProdutoResponseDTO(1L, inputDTO.getNome(), inputDTO.getDescricao(), inputDTO.getValor());
-        return ResponseEntity.created(null).body(dto);
-    }
 }
